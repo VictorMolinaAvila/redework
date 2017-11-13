a53039b7964bd9efec33e378a663097ff5c0a32d
name <a@a>
2017-11-12 22:41:43 -0200

    evelop

---
+++ client/app/core/userController.js
@@ -0,0 +1,80 @@
+(function () {
+    'use strict';
+
+    angular.module('app')
+        .controller('userController', ['$scope','$stateParams', '$state', '$window','userService', userController])
+        .service('userService', function($http, $window) {
+
+ 
+    this.user = function(id_token){
+        $http({
+        url: 'https://redework.auth0.com/tokeninfo',
+        method: "POST",
+        data: {'id_token':id_token}
+    })
+    .then(function(response) {
+
+    	console.log(response.data);
+    	var id = response.data.user_id;
+    	var nome = response.data.name;
+    	var email = response.data.email;
+    	var foto; 
+    	if(response.data.identities[0].connection == "redework"){
+    		var foto = response.data.picture;
+    		console.log("redework"+foto);
+    	}
+
+    	if(response.data.identities[0].connection == "google-oauth2"){
+    		var foto = response.data.picture;
+    		console.log("google"+foto);
+    	}
+
+    	if(response.data.identities[0].connection == "linkedin"){
+    		var foto = response.data.pictureUrls.values[0];
+    		console.log("linkedin"+foto);
+    	}
+
+    	if(response.data.identities[0].connection == "facebook"){
+			var foto = response.data.picture_large;
+			console.log("face"+foto);
+    	}
+
+
+        $window.sessionStorage.clear();
+        window.sessionStorage.setItem('user_id', id);
+
+    	$http({
+        url: 'http://localhost:8090/candidato',
+        method: "POST",
+        data: {"user_id":id , "nome":nome, "email":email, "foto":foto}
+    })
+    .then(function(response) {
+
+    },
+    function(response) { // optional
+            // failed
+    });
+
+
+    },
+    function(response) { // optional
+            // failed
+    });
+    
+    }
+
+});
+
+    function userController($scope, $stateParams, $state, $window, userService) {
+         userService.user($stateParams.id_token);
+
+
+ 
+        $state.go('page/profile');
+
+
+
+        //var usuario = window.sessionStorage.getItem('usuario');
+    }
+
+})(); 
---
+++ client/app/table/table.controller.js
@@ -0,0 +1,142 @@
+(function () {
+    'use strict';
+
+    angular.module('app.table')
+        .controller('nutritionController', ['$mdEditDialog', '$q', '$scope', '$timeout', 'tabelaService', nutritionController])
+        .service('tabelaService', function($http, $window) {
+
+    this.todasVagas = function(){
+    return $http.get("http://localhost:8090/vaga/");
+  }
+
+  this.todosCurriculos = function(){
+    return $http.get("http://localhost:8090/curriculo/");
+  }
+  });
+
+    function nutritionController($mdEditDialog, $q, $scope, $timeout, tabelaService) {
+        $scope.selected = [];
+        $scope.limitOptions = [5, 10, 15];
+        
+        $scope.options = {
+            rowSelection: true,
+            multiSelect: true,
+            autoSelect: true,
+            decapitate: false,
+            largeEditDialog: false,
+            boundaryLinks: false,
+            limitSelect: true,
+            pageSelect: true
+        };
+        
+        $scope.query = {
+            order: 'descricao',
+            limit: 5,
+            page: 1
+        };
+
+
+         tabelaService.todasVagas().then(function(d){
+
+          $scope.vagas = d.data;
+          
+           tabelaService.todosCurriculos().then(function(d){
+                 $scope.curriculos = d.data;
+            });
+      });
+
+
+          $scope.teste = function () {
+           console.log("teste");
+        };
+
+
+
+
+
+        $scope.desserts = {
+            "count": 9,
+            "data": [
+                {
+                    "name": "Frozen yogurt",
+                    "type": "Ice cream",
+                    "calories": { "value": 159.0 },
+                    "fat": { "value": 6.0 },
+                    "carbs": { "value": 24.0 },
+                    "protein": { "value": 4.0 },
+                    "sodium": { "value": 87.0 },
+                    "calcium": { "value": 14.0 },
+                    "iron": { "value": 1.0 }
+                }, 
+            ]
+        };
+        
+        $scope.editComment = function (event, dessert) {
+            event.stopPropagation(); // in case autoselect is enabled
+            
+            var editDialog = {
+                modelValue: dessert.comment,
+                placeholder: 'Add a comment',
+                save: function (input) {
+                    if(input.$modelValue === 'Donald Trump') {
+                        input.$invalid = true;
+                        return $q.reject();
+                    }
+                    if(input.$modelValue === 'Bernie Sanders') {
+                        return dessert.comment = 'FEEL THE BERN!'
+                    }
+                    dessert.comment = input.$modelValue;
+                },
+                targetEvent: event,
+                title: 'Add a comment',
+                validators: {
+                    'md-maxlength': 30
+                }
+            };
+            
+            var promise;
+            
+            if($scope.options.largeEditDialog) {
+                promise = $mdEditDialog.large(editDialog);
+            } else {
+                promise = $mdEditDialog.small(editDialog);
+            }
+            
+            promise.then(function (ctrl) {
+                var input = ctrl.getInput();
+                
+                input.$viewChangeListeners.push(function () {
+                    input.$setValidity('test', input.$modelValue !== 'test');
+                });
+            });
+        };
+        
+        $scope.toggleLimitOptions = function () {
+            $scope.limitOptions = $scope.limitOptions ? undefined : [5, 10, 15];
+        };
+        
+        $scope.getTypes = function () {
+            return ['Candy', 'Ice cream', 'Other', 'Pastry'];
+        };
+        
+        $scope.loadStuff = function () {
+            $scope.promise = $timeout(function () {
+                // loading
+            }, 2000);
+        }
+        
+        $scope.logItem = function (item) {
+            console.log(item.name, 'was selected');
+        };
+        
+        $scope.logOrder = function (order) {
+            console.log('order: ', order);
+        };
+        
+        $scope.logPagination = function (page, limit) {
+            console.log('page: ', page);
+            console.log('limit: ', limit);
+        }
+    }
+
+})(); 
